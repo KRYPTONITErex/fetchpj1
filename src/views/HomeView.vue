2dd4dd25d3e3e507712152076f9de3e9f860f25b
Lwin Phyo <lwinphyo271395@gmail.com>
2024-11-09 13:44:45 +0700

    1st commit fetch by myWork

---
+++ src/views/HomeView.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="home">
+
+    <h1>THIS IS HOME PAGE</h1>
+
+    <h3>Testing and practing the FETCH,.then and .catch HERE</h3>
+
+  </div>
+</template>
+
+<script>
+
+  fetch("/database/db.json")
+
+  .then((response)=>{
+    // console.log("resolved", response.json);
+    
+    if (response.status===404){
+      throw new Error("ur link no good")
+    }
+    return response.json();
+  })
+  .then((data)=>{
+    console.log(data);
+  })
+  .catch((err)=>{
+    console.log(err.message);
+  })
+
+  // fetch("/database/db.json")
+
+  // .then((response)=>{
+  //   return response.json();
+  // })
+  // .then((data)=>{
+  //   console.log(data);
+  // })
+  // .catch((err)=>{
+  //   console.log(err);
+  // })
+
+  // let getJson = async()=>{
+  //   let response = await fetch("/database/db.json");
+
+  //   let datas = await response.json();
+  //   // console.log(datas);
+  //   return datas;
+
+  // }
+  // getJson()
+  // .then((datas)=>{
+  //   console.log(datas);
+  // })
+
+  let getmyJson = async()=>{
+    let response = await fetch("/database/db.json")
+
+    let datas = await response.json();
+    return datas;
+
+  }
+  getmyJson()
+  .then((datas)=>{
+    console.log(datas);
+  })
+
+
+
+  export default {
+    name: 'HomeView',
+    components: {
+      
+    }
+  }
+
+</script>
